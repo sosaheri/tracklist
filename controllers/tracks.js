8c454f715c1cf1a9baf2b2fdf8303f6116c05da3
Heriberto <sosaheriberto2001@gmail.com>
2022-11-24 07:52:54 -0400

    gestion de errores

--- controllers/tracks.js
+++ controllers/tracks.js
@@ -1,21 +1,55 @@
 const { tracksModel } = require('../models');
+const { matchedData } = require('express-validator');
+const  { handleHttpError } = require('../helpers/handlerError');
 
 const getItems = async (req, res) => {
-    const data = await tracksModel.find({});
-    
-    res.send({data});
+
+    try {
+        const data = await tracksModel.find({});
+        res.send({data});
+        
+    } catch (e) {
+        handleHttpError(res, 'ERROR_GET_ITEMS')
+    }
+
 };
 
-const getItem = (req, res) => {};
+const getItem = (req, res) => {
+
+    try {
+       
+        
+    } catch (e) {
+        handleHttpError(res, 'ERROR_GET_ITEM')
+    }
+};
 
 const createItem = async (req, res) => {
-    const { body } = req
-    // console.log(body)
-    const data = await tracksModel.create(body);
-    res.send({data})
+    try {
+        const body = matchedData(req);
+
+        // const { body } = req
+        // console.log(body)
+        const data = await tracksModel.create(body);
+        res.send({data})        
+    } catch (e) {
+        handleHttpError(res, 'ERROR_CREATE_ITEMS')
+    }
 
 };
-const updateItem = (req, res) => {};
-const deleteItem = (req, res) => {};
+const updateItem = (req, res) => {
+    try {       
+        
+    } catch (e) {
+        handleHttpError(res, 'ERROR_UPDATE_ITEM')
+    }
+};
+const deleteItem = (req, res) => {
+    try {       
+        
+    } catch (e) {
+        handleHttpError(res, 'ERROR_DELETE_ITEMS')
+    }
+};
 
 module.exports = { getItems, getItem, createItem, updateItem, deleteItem };
